ba22c206e2d2d0a2fa79738283a83f4e2c466238
Billy <ouattarabilly33@hotmail.com>
2022-12-25 13:59:18 -0800

    initial

--- src/App.js
+++ src/App.js
@@ -8,15 +8,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Home />
-          {/* <BrowserRouter>
-            <Routes>
-                <Route index element={<Home />} />
-                <Route path="contact" element={<Contact />} />
-                <Route path="about" element={<About />} />
-                //{<Route path="*" element={<NoPage />} /> }
-            </Routes> 
-            </BrowserRouter> */}
+      <BrowserRouter>
+        <Routes>
+            <Route index element={<Home />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="about" element={<About />} />
+            {/*<Route path="*" element={<NoPage />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
